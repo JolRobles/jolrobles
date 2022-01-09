b6d40fb5a5fe63feb0ba5016893c6d0116d5deb7
JolRobles <31939604+JolRobles@users.noreply.github.com>
2022-01-09 02:36:13 -0500

    config heroku

--- jolrobles/settings.py
+++ jolrobles/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+django_heroku.settings(locals())
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
