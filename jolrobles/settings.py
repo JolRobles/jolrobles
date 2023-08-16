ca087386bfa138c82e040984085d6d7725e6f090
JolRobles <jluis4013@gmail.com>
2023-08-15 22:54:32 -0500

    database

--- jolrobles/settings.py
+++ jolrobles/settings.py
@@ -79,26 +79,26 @@ WSGI_APPLICATION = 'jolrobles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'jolrobles',
-        'USER': 'postgres',
-        'PASSWORD': 'root', #yorch
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
-}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #         'NAME': 'jolrobles',
-#         'USER': 'admin',
-#         'PASSWORD': 'GU8mXA8wcyheOtPsY6d7YCjhIm9jDLdG', #yorch
-#         'HOST': 'dpg-cj85dttjeehc73a65vag-a',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root', #yorch
+#         'HOST': 'localhost',
 #         'PORT': 5432,
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jolrobles',
+        'USER': 'admin',
+        'PASSWORD': 'GU8mXA8wcyheOtPsY6d7YCjhIm9jDLdG', #yorch
+        'HOST': 'dpg-cj85dttjeehc73a65vag-a',
+        'PORT': 5432,
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
